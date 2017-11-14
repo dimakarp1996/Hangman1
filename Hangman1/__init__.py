8f2305815d086a862e68fcb757837d728747d57f
dimakarp1996 <dimakarp1996@yandex.ru>
2017-11-14 18:10:13 +0300

    Add files via upload

--- Hangman1/__init__.py
+++ Hangman1/__init__.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 10:59:51 2017
+@author: tcs-user
+"""
 
 import random  # calculate random numbers
-DICT = ['hello', 'apple', 'phone', 'smell', 'brink']
+DICT = ['east', 'eats', 'seat', 'teas']
 
 
 def loss(attempts, num):  # calculate loss
@@ -15,27 +21,47 @@ def check(inp, guess, letter, attempts):
     if inp in letter:
         for j in range(length):
             if letter[j] == inp:
-                guess = guess[:j] + inp + guess[j+1:]
+                guess = guess[:j] + inp + guess[j + 1:]
         print('Hit!')
-        print('The word: '+guess)
+        print('The word: ' + guess)
     else:
         attempts += 1
         print('Nussed, mistake %d out of %d' % (attempts, length))
     return guess, letter, attempts
 
 
-def play():
+def get_input():
+    var = input()
+    return var[0]
+
+
+def gener_random():
     random.seed(0)
-    i = random.randint(0, 4)
+    i = random.randint(0, 3)
     print("GUESS a LETTER:")
+    return i
+
+
+def gener_init(i):
     letter = DICT[i]
     length = len(letter)
-    guess = '*'*length
+    guess = '*' * length
     attempts = 0
-    while attempts < length:
-        inp = input()
-        inp = inp[0]
+    return letter, length, guess, attempts
+
+
+def play():
+    i = gener_random()
+    letter, length, guess, attempts = gener_init(i)
+    while attempts < length and guess != letter:
+        inp = get_input()
         guess, letter, attempts = check(inp, guess, letter, attempts)
-        if guess == letter:
-            break
-    print(loss(attempts, length)+'\n')
+    print(loss(attempts, length))
+    return 0
+
+
+def main():
+    a = play()
+    print(a)
+    return 0
+
